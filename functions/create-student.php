98d780314b0206029bed870f0cdb90cf016912b9
HashJProgramming <joshuamarceloambalong@gmail.com>
2023-06-10 00:05:23 +0800

    Version 5.1 - Minor Fix

--- functions/create-student.php
+++ functions/create-student.php
@@ -13,36 +13,34 @@ $phone = $_POST['guardian_phone'];
 // get the age from the birthday
 $age =  date_diff(date_create($birthday), date_create('today'))->y;
 
-// check if the student already exists
-$sql = "SELECT * FROM students WHERE fullname = :fullname AND id = :id";
+// check if student id or fullname already exists
+$sql = "SELECT * FROM students WHERE id = :id OR fullname = :fullname";
 $statement = $db->prepare($sql);
-$statement->bindParam(':fullname', $fullname);
 $statement->bindParam(':id', $id);
+$statement->bindParam(':fullname', $fullname);
+$statement->execute();
+$student = $statement->fetch(PDO::FETCH_ASSOC);
+
+// if student id or fullname already exists, redirect to the index page 
+if ($student) {
+    header('Location: ../dashboard.php#error');
+    exit();
+}
+
+
+$sql = "INSERT INTO students (id, fullname, birthday, age, sex, strand, guardian_name, phone)
+        VALUES (:id, :fullname, :birthday, :age, :sex, :strand, :guardian_name, :phone)";
+$statement = $db->prepare($sql);
+$statement->bindParam(':id', $id);
+$statement->bindParam(':fullname', $fullname);
+$statement->bindParam(':birthday', $birthday);
+$statement->bindParam(':age', $age);
+$statement->bindParam(':sex', $sex);
+$statement->bindParam(':strand', $strand);
+$statement->bindParam(':guardian_name', $guardian_name);
+$statement->bindParam(':phone', $phone);
 $statement->execute();
 
-// if the student already exists, redirect to the index page
-if ($statement->fetchColumn() > 0) {
-    header('Location: dashboard.php#error');
-    exit();
-}
-
-// if the student does not already exist, proceed with adding them
-else {
-    // prepare and execute the SQL query
-    $sql = "INSERT INTO students (id, fullname, birthday, age, sex, strand, guardian_name, phone)
-            VALUES (:id, :fullname, :birthday, :age, :sex, :strand, :guardian_name, :phone)";
-    $statement = $db->prepare($sql);
-    $statement->bindParam(':id', $id);
-    $statement->bindParam(':fullname', $fullname);
-    $statement->bindParam(':birthday', $birthday);
-    $statement->bindParam(':age', $age);
-    $statement->bindParam(':sex', $sex);
-    $statement->bindParam(':strand', $strand);
-    $statement->bindParam(':guardian_name', $guardian_name);
-    $statement->bindParam(':phone', $phone);
-    $statement->execute();
-
-    // redirect to the index page after adding the student
-    header('Location: ../dashboard.php#success');
-    exit();
-}
+// redirect to the index page after adding the student
+header('Location: ../dashboard.php#success');
+exit();
